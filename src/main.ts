0161b354c6814aaee6f3238960cdf05f603f7d91
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-03-04 16:59:52 +0300

    настройка окружения

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import App from './App.svelte'
 
 const app = new App({
-  target: document.getElementById('app') as Document | Element | ShadowRoot
+  target: document.getElementById('app')!
 })
 
 export default app;
