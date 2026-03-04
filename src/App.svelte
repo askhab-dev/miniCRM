d8fa87c301d2efe5aeccb82bc34c83e1c86e146f
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-03-04 20:16:32 +0300

    App.svelte + @sveltestack/svelte-query

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
-  // TODO: Implement application
+  import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query'
+  import Footer from './components/Footer.svelte';
+  import Header from './components/Header.svelte';
+  import ClientsList from './components/ClientsList.svelte';
+
+  const queryClient = new QueryClient()
 </script>
 
-<main>
-  <h1>miniCRM</h1>
-</main>
+<QueryClientProvider client={queryClient}>
+  <main class="app">
+    <Header />
+
+    <div class="layout">
+      <ClientsList />
+    </div>
+
+    <Footer />
+  </main>
+</QueryClientProvider>
+
+<style>
+  .app {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .layout {
+    display: flex;
+    flex: 1;
+    max-width: 1400px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 24px;
+  }
+
+</style>
