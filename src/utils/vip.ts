99fc399c3bb89033cc87e7f9db0792de4c59e9fa
aabdulkadirov <aabdulkadirov@team.amocrm.com>
2026-03-05 11:46:21 +0300

    isVip logic

--- src/utils/vip.ts
+++ src/utils/vip.ts
@@ -2,7 +2,9 @@ import type { Client } from '../types/client'
 import { getValidDate, isMoreThan30Days } from './date';
 
 export function isVip(client: Client): boolean {
-  if (Number(client.balance) < 1001) {
+  const balance = Number(client.balance);
+
+  if (isNaN(balance) || balance < 1001) {
     return false;
   }
 
